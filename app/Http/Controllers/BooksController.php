273e11a79debb8549ab7f6d9d6f7677a60a84648
carlosrp <carlosreyplanelles@gmail.com>
2018-02-27 20:19:45 +0100

    Validaciones

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -34,14 +34,62 @@ class booksController extends Controller
     {
         $mytime = Carbon::now();
         $book= new Libro;
-        $book->isbn=$request->input('isbn');
-        $book->voto=$request->input('voto');
-        $book->num_voto=$request->input('num_voto');
-        $book->n_pags=$request->input('n_pags');
-        $book->precio=$request->input('precio');
-        $book->titulo=$request->input('titulo');
-        $book->editorial=$request->input('editorial');
-        $book->atributos_extra=$request->input('atributos_extra');
+        if(gettype($request->input('isbn'))==NULL ||gettype($request->input('isbn'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->isbn=$request->input('isbn');
+        }
+
+        if(gettype($request->input('voto'))==NULL || gettype($request->input('voto'))!='integer') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->voto=$request->input('voto');
+        }
+
+        if(gettype($request->input('num_voto'))==NULL || gettype($request->input('num_voto'))!='integer') {
+            throw new HttpException(400, "Invalid data");
+
+        }else {
+            $book->num_voto=$request->input('num_voto');
+        }
+
+        if(gettype($request->input('n_pags'))==NULL || gettype($request->input('n_pags'))!='integer'){
+            throw new HttpException(400, "Invalid data");
+
+        }else {
+            $book->n_pags=$request->input('n_pags');
+        }
+
+        if(gettype($request->input('precio'))==NULL || gettype($request->input('precio'))!='double'){
+            throw new HttpException(400, "Invalid data");
+
+        }else{
+            $book->precio=$request->input('precio');
+        }
+
+        if(gettype($request->input('titulo'))==NULL ||gettype($request->input('titulo'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->titulo=$request->input('titulo');
+        }
+
+        if(gettype($request->input('editorial'))==NULL ||gettype($request->input('editorial'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->editorial=$request->input('editorial');
+        }
+
+        if(gettype($request->input('atributos_extra'))==NULL ||gettype($request->input('atributos_extra'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->atributos_extra=$request->input('atributos_extra');
+        }
+
         $book->created_at= $mytime;
 
         if($book->save()){
@@ -73,15 +121,62 @@ class booksController extends Controller
 
         $book = Libro::find($request->id);
         $mytime = Carbon::now();
+        if(gettype($request->input('isbn'))==NULL ||gettype($request->input('isbn'))!='string') {
+            throw new HttpException(400, "Invalid data");
 
-        $book->isbn = $request->input('isbn');
-        $book->voto = $request->input('voto');
-        $book->num_voto = $request->input('num_voto');
-        $book->n_pags = $request->input('n_pags');
-        $book->precio = $request->input('precio');
-        $book->titulo = $request->input('titulo');
-        $book->editorial = $request->input('editorial');
-        $book->atributos_extra = $request->input('atributos_extra');
+        } else {
+            $book->isbn=$request->input('isbn');
+        }
+
+        if(gettype($request->input('voto'))==NULL || gettype($request->input('voto'))!='integer') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->voto=$request->input('voto');
+        }
+
+        if(gettype($request->input('num_voto'))==NULL || gettype($request->input('num_voto'))!='integer') {
+            throw new HttpException(400, "Invalid data");
+
+        }else {
+            $book->num_voto=$request->input('num_voto');
+        }
+
+        if(gettype($request->input('n_pags'))==NULL || gettype($request->input('n_pags'))!='integer'){
+            throw new HttpException(400, "Invalid data");
+
+        }else {
+            $book->n_pags=$request->input('n_pags');
+        }
+
+        if(gettype($request->input('precio'))==NULL || gettype($request->input('precio'))!='double'){
+            throw new HttpException(400, "Invalid data");
+
+        }else{
+            $book->precio=$request->input('precio');
+        }
+
+        if(gettype($request->input('titulo'))==NULL ||gettype($request->input('titulo'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->titulo=$request->input('titulo');
+        }
+
+        if(gettype($request->input('editorial'))==NULL ||gettype($request->input('editorial'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->editorial=$request->input('editorial');
+        }
+
+        if(gettype($request->input('atributos_extra'))==NULL ||gettype($request->input('atributos_extra'))!='string') {
+            throw new HttpException(400, "Invalid data");
+
+        } else {
+            $book->atributos_extra=$request->input('atributos_extra');
+        }
+
         $book->updated_at = $mytime;
 
         if ($book->save()) {
