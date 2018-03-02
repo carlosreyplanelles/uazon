27be5a8fdc3b02aa2e5278ca92735cf32d2ea6b5
carlosrp <carlosreyplanelles@gmail.com>
2018-03-02 12:45:10 +0100

    Validacion de API funcional

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -35,7 +35,7 @@ class booksController extends Controller
     public function store(Request $request)
     {
         $rules= [
-            'isbn' => 'required|string|size:10|size:13',
+            'isbn' => 'required|integer|digits:9',
             'n_pags' => 'required|integer|min:1',
             'precio' => 'required|numeric|min:1',
             'titulo' => 'required|string|max:255',
@@ -45,23 +45,25 @@ class booksController extends Controller
 
         $messages=[
             'required' => 'El campo :attribute es obligatorio',
+            'digits' => 'La longitud del campo :attribute es incorrecta',
             'string' => 'Formato de :attribute incorrecto. Se esperaba una cadena.',
             'integer' => 'Formato de :attribute incorrecto. Se esperaba un número entero.',
             'json' => 'Formato de :attribute incorrecto. Se esperaba una cadena json válida',
             'min:1' => 'Valor de :attribute incorrecto. Se esperaba un valor positivo',
             'max:255' => 'El valor de :attribute es demasiado largo'
             ];
-        echo $request->getContentType();
-        $validator=Validator::make($request->getContent(), $rules, $messages);
-        $errors = $validator->errors();
+
+        $validator=Validator::make($request->input(), $rules, $messages);
+
         if ($validator->fails()) {
-            foreach ($validator->errors() as $v){
-                echo $v;
+            $errors=$validator->errors();
+            foreach ($errors->all() as $message) {
+                echo $message.'<br>';
             }
             exit();
         }
 
-        $mytime = Carbon::now();
+        //$mytime = Carbon::now();
         $book= new Libro;
         $book->isbn=$request->input('isbn');
         $book->n_pags=$request->input('n_pags');
@@ -71,11 +73,11 @@ class booksController extends Controller
         $book->atributos_extra=$request->input('atributos_extra');
         $book->voto=0;
         $book->num_voto=0;
-        $book->created_at= $mytime;
-        return $book;
-        /*if($book->save()){
+        //$book->created_at= $mytime;
 
-        }*/
+        if($book->save()){
+            return $book;
+        }
 
     }
 
@@ -99,74 +101,72 @@ class booksController extends Controller
     public function update(Request $request)
     {
 
-        $book = Libro::find($request->id);
-        $mytime = Carbon::now();
-        if(gettype($request->input('isbn'))==NULL ||gettype($request->input('isbn'))!='string') {
-            throw new HttpException(400, "Invalid data");
+        $rules= [
+            'isbn' => 'integer|digits:9',
+            'n_pags' => 'integer|min:1',
+            'precio' => 'numeric|min:1',
+            'titulo' => 'string|max:255',
+            'editorial' => 'string',
+            'atributos_extra' => 'json'
+        ];
 
-        } else {
+        $messages=[
+            'digits' => 'La longitud del campo :attribute es incorrecta',
+            'string' => 'Formato de :attribute incorrecto. Se esperaba una cadena.',
+            'integer' => 'Formato de :attribute incorrecto. Se esperaba un número entero.',
+            'json' => 'Formato de :attribute incorrecto. Se esperaba una cadena json válida',
+            'min:1' => 'Valor de :attribute incorrecto. Se esperaba un valor positivo',
+            'max:255' => 'El valor de :attribute es demasiado largo'
+        ];
+
+        $validator=Validator::make($request->input(), $rules, $messages);
+
+        if ($validator->fails()) {
+            $errors=$validator->errors();
+            foreach ($errors->all() as $message) {
+                echo $message.'<br>';
+            }
+            exit();
+        }
+
+
+        $book = Libro::find($request->id);
+        if(gettype($request->input('isbn'))!=NULL){
             $book->isbn=$request->input('isbn');
         }
-
-        if(gettype($request->input('voto'))==NULL || gettype($request->input('voto'))!='integer') {
-            throw new HttpException(400, "Invalid data");
-
-        } else {
+        if(gettype($request->input('voto'))!=NULL ){
             $book->voto=$request->input('voto');
         }
-
-        if(gettype($request->input('num_voto'))==NULL || gettype($request->input('num_voto'))!='integer') {
-            throw new HttpException(400, "Invalid data");
-
-        }else {
+        if(gettype($request->input('num_voto'))!=NULL ) {
             $book->num_voto=$request->input('num_voto');
         }
-
-        if(gettype($request->input('n_pags'))==NULL || gettype($request->input('n_pags'))!='integer'){
-            throw new HttpException(400, "Invalid data");
-
-        }else {
+        if(gettype($request->input('n_pags'))!=NULL){
             $book->n_pags=$request->input('n_pags');
         }
-
-        if(gettype($request->input('precio'))==NULL || gettype($request->input('precio'))!='double'){
-            throw new HttpException(400, "Invalid data");
-
-        }else{
+        if(gettype($request->input('precio'))!=NULL){
             $book->precio=$request->input('precio');
         }
 
-        if(gettype($request->input('titulo'))==NULL ||gettype($request->input('titulo'))!='string') {
-            throw new HttpException(400, "Invalid data");
-
-        } else {
+        if(gettype($request->input('titulo'))!=NULL){
             $book->titulo=$request->input('titulo');
         }
 
-        if(gettype($request->input('editorial'))==NULL ||gettype($request->input('editorial'))!='string') {
-            throw new HttpException(400, "Invalid data");
-
-        } else {
+        if(gettype($request->input('editorial'))!=NULL){
             $book->editorial=$request->input('editorial');
         }
 
-        if(gettype($request->input('atributos_extra'))==NULL ||gettype($request->input('atributos_extra'))!='string') {
-            throw new HttpException(400, "Invalid data");
-
-        } else {
+        if(gettype($request->input('atributos_extra'))!=NULL ){
             $book->atributos_extra=$request->input('atributos_extra');
         }
-
-        $book->updated_at = $mytime;
-
         if ($book->save()) {
             return $book;
         }
-
-        throw new HttpException(400, "Invalid data");
     }
 
     public function comments($id){
+        if (Libro::find($id)==null){
+            abort(404, 'error 404: recurso no encontrado.');
+        }
         $comments = Comentarios::where('fk_libros', $id)->get();
         foreach ($comments as $c){
             echo $c->descripcion;
